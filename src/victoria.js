5b0cc817bbe33c56b22743f216bd4b4a1c56f741
Vinícius Roggia Gomes <vinicius@intellead.com>
2018-12-11 11:36:28 -0200

    removed unused parameter

--- src/victoria.js
+++ src/victoria.js
@@ -19,7 +19,7 @@ var request = require('request');
 
 module.exports = {
 
-    send_the_lead_to_victoria: function(lead, origem) {
+    send_the_lead_to_victoria: function(lead) {
         var data = {
             "leads":[{
                 "id_rd": lead.id,
